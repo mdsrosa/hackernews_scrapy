8aa6e517956fc49e2e8a5bed0c574ad30379f114
Matheus Rosa <matheusdsrosa@gmail.com>
2015-11-25 11:48:49 -0200

    Add filter in titles

--- hackernews_scrapy/spiders/hackernews_spider.py
+++ hackernews_scrapy/spiders/hackernews_spider.py
@@ -1,20 +1,26 @@
 from scrapy.spiders import Spider
-from scrapy.http import Request
 from hackernews_scrapy.items import HackernewsScrapyItem
 from scrapy.selector import Selector
 
+import datetime
+
 
 class HackernewsSpider(Spider):
-	name = 'pythonhackernews'
-	allowed_hosts = ['news.ycombinator.com']
-	start_urls = ['https://news.ycombinator.com']
-	
-	def parse(self, response):
-		"""Parse the HTML to get the information we need"""
-		html_xpath_selector = Selector(response)
-		titles = html_xpath_selector.xpath('//td[@class="title"]/a/text()').extract()
-		
-		for title in titles:
-			item = HackernewsScrapyItem()
-			item['title'] = title
-			yield item
+    name = 'pythonhackernews'
+    allowed_hosts = ['news.ycombinator.com']
+    start_urls = ['https://news.ycombinator.com']
+
+    def parse(self, response):
+        """Parse the HTML to get the information we need"""
+        html_xpath_selector = Selector(response)
+        titles = html_xpath_selector.xpath(
+            '//td[@class="title"]/a/text()').extract()
+
+        titles = filter(lambda t: 'python' in t.lower(), titles)
+
+        for title in titles:
+            item = HackernewsScrapyItem()
+            item['title'] = title
+            item['crawled_at'] = datetime.datetime.utcnow()
+
+            yield item
